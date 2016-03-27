f853830387eb8ac0fb7f579c7b025d62115d7072
marzim <marvin.casagnap@gmail.com>
2016-03-27 11:25:39 +0800

    commit asp.net mvc 5

---
+++ PersitentUniversity/PersitentUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PersitentUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PersitentUniversity.DAL
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
